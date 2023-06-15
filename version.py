398583ec765f7666abacdacbb7e8858fe961465d
Erik-Kolb <125336455+Erik-Kolb@users.noreply.github.com>
2023-06-15 10:07:31 -0400

    Create version.py

---
+++ version.py
@@ -0,0 +1,30 @@
+import os
+cat = "cat"
+
+def list_directory_contents(directory):
+    # Get the contents of the directory
+    contents = os.listdir(directory)
+
+    # Iterate over the contents
+    for item in contents:
+        item_path = directory + '/' + item  # Construct the item path
+        if item == "VERSION":
+            print(os.system(cat + " " + item_path))
+            break
+        # Check if the item is a directory
+        is_directory = False
+        try:
+            # In some MicroPython implementations, this may need to be adjusted
+            is_directory = os.stat(item_path)[0] & 0x4000 == 0x4000
+        except Exception as e:
+            pass
+
+        # If the item is a directory, recursively call the function
+        if is_directory:
+            list_directory_contents(item_path)
+
+# Provide the starting directory here
+starting_directory = '/'
+
+# Call the function with the starting directory
+list_directory_contents(starting_directory)
