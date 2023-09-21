295897cb9eee8df08e2b52c1b2600e92b6a2a857
Erik-Kolb <125336455+Erik-Kolb@users.noreply.github.com>
2023-09-21 00:42:17 -0400

    Create Harness.c

---
+++ Harness.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <string.h>
+#include <studio.h>
+
+#indlude "ISOMovies.h"
+
+
+int main(char* argv[]) {
+	if(strcmp(argv[1], "MJ2SetMovieMatrix") == 0 {
+		
+		ssize_t length;
+		length = read(STDIN_FILEND, input)
+		a = u32
+		MJ2SetMovieMatrix(length, a):
+	}
+}
+		
+		
